bef620d8878aa4c9051455868b198eeebf00ce63
anubhavsingh16 <anubhavssingh177@gmail.com>
2020-05-15 13:40:04 +0530

    _

---
+++ OpenSourceEvents-iOS/OpenSourceEvents-iOS/secondPageUiController.swift
@@ -0,0 +1,30 @@
+//
+//  secondPageUiController.swift
+//  OpenSourceEvents-iOS
+//
+//  Created by Anubhav Singh on 15/05/20.
+//  Copyright © 2020 Catalyst. All rights reserved.
+//
+
+import UIKit
+
+class secondPageUiController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
