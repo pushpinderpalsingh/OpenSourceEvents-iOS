8d0b9da32252fa4694ac84c620edde92d12218c9
Pushpinder Pal Singh <pushpinder@tutamail.com>
2020-06-02 10:50:41 +0530

    UAdd Animation

--- OpenSourceEvents-iOS/OpenSourceEvents-iOS/FirstPageUIController.swift
+++ OpenSourceEvents-iOS/OpenSourceEvents-iOS/FirstPageUIController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class FirstPageUIController: UIViewController {
-
+    
     @IBOutlet weak var numofevents: UILabel!
     @IBOutlet weak var sourceDefi: UITextView!
     @IBOutlet weak var hackathonDefi: UITextView!
     
     @IBOutlet weak var currentMonth: UITextView!
+    @IBOutlet weak var titleName: UILabel!
     
     
     override func viewDidLoad() {
@@ -25,21 +26,42 @@ class FirstPageUIController: UIViewController {
         
         
         let currentMonthEvent = NSMutableAttributedString(string: "Open Source Events:  \nGSoC\nKWoC\nGCI\nAlibaba SoC\nHackathons: \nSIH\npycon\n\n")
-
+        
         let attributes0: [NSAttributedString.Key : Any] = [
-           .font: UIFont(name: "HelveticaNeue-Bold", size: 15)!
+            .font: UIFont(name: "HelveticaNeue-Bold", size: 15)!
         ]
         attributedString.addAttributes(attributes0, range: NSRange(location: 0, length: 19))
-
+        
         let attributes9: [NSAttributedString.Key : Any] = [
-           .font: UIFont(name: "HelveticaNeue-Bold", size: 15)!
+            .font: UIFont(name: "HelveticaNeue-Bold", size: 15)!
         ]
         attributedString.addAttributes(attributes9, range: NSRange(location: 48, length: 12))
         
         currentMonth.attributedText = currentMonthEvent
         
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        titleName.alpha = 0
+        fadeViewInThenOut(view: titleName, delay: 0.4)
+    }
+    func fadeViewInThenOut(view : UIView, delay: TimeInterval) {
+        
+        let animationDuration = 0.7
+        
+        // Fade in the view
+        UIView.animate(withDuration: animationDuration, animations: { () -> Void in
+            view.alpha = 0
+        }) { (Bool) -> Void in
+            
+            // After the animation completes, fade out the view after a delay
+            
+            UIView.animate(withDuration: animationDuration, delay: delay, options: .curveEaseInOut, animations: { () -> Void in
+                view.alpha = 1
+            },
+                                       completion: nil)
+        }
+    }
+    
     @IBAction func mailButtonPressed(_ sender: UIButton) {
         if let url = URL(string: "mailto:catalystSMVD@hotmail.com") {
             UIApplication.shared.open(url)
@@ -47,8 +69,8 @@ class FirstPageUIController: UIViewController {
     }
     @IBAction func githubButtonPressed(_ sender: UIButton) {
         if let url = URL(string: "https://github.com/Catalyst-SMVD") {
-                   UIApplication.shared.open(url)
-               }
+            UIApplication.shared.open(url)
+        }
     }
     
     @IBAction func linkedinButtonPressed(_ sender: UIButton) {
--- OpenSourceEvents-iOS/OpenSourceEvents-iOS/secondPageUiController.swift
+++ OpenSourceEvents-iOS/OpenSourceEvents-iOS/secondPageUiController.swift
@@ -9,14 +9,35 @@
 import UIKit
 
 class secondPageUiController: UIViewController {
-
+    
     @IBOutlet weak var gifview: UIImageView!
+    @IBOutlet weak var titleName: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        titleName.alpha = 0
         gifview.loadGif(name: "assignment")
     }
+    override func viewDidAppear(_ animated: Bool) {
+        
+        fadeViewInThenOut(view: titleName, delay: 0.4)
+    }
+    func fadeViewInThenOut(view : UIView, delay: TimeInterval) {
+        
+        let animationDuration = 0.7
+        
+        // Fade in the view
+        UIView.animate(withDuration: animationDuration, animations: { () -> Void in
+            view.alpha = 0
+        }) { (Bool) -> Void in
+            
+            // After the animation completes, fade out the view after a delay
+            
+            UIView.animate(withDuration: animationDuration, delay: delay, options: .curveEaseInOut, animations: { () -> Void in
+                view.alpha = 1
+            },
+                           completion: nil)
+        }
+    }
     
-
 }
